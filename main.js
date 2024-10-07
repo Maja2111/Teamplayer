8d07cbe46c89812683fb33ff83d1129660b7ee52
Sabine <sabine.weber@dci-student.org>
2024-10-07 12:44:26 +0200

    final result

--- main.js
+++ main.js
@@ -19,3 +19,36 @@ async function sleep(ms) {
   console.log('Hey');
 }
 sleep(2000);
+
+//Task 3
+console.log('');
+class Dog {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const chrissisDog = new Dog('Fridolin', 3);
+const dannyDog = new Dog('Astera', 5);
+const bastisDog = new Dog('Pluto', 2);
+const binesDog = new Dog('Peggy', 15);
+
+console.log(chrissisDog);
+console.log(dannyDog);
+console.log(bastisDog);
+console.log(binesDog);
+
+//Task 4
+async function fetchDogBreeds() {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/list/all');
+    const data = await response.json();
+    console.log('Hunderassen:');
+    console.log(Object.keys(data.message));
+  } catch (error) {
+    console.error('Fehler bei der Anfrage:', error);
+  }
+}
+
+fetchDogBreeds();
